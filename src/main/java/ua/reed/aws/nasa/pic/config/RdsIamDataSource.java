17437f4403f0442455492be3cbb840f009030de8
vromantsev <v.romantsev@ocado.com>
2024-03-07 23:14:21 +0200

    changed configuration

--- src/main/java/ua/reed/aws/nasa/pic/config/RdsIamDataSource.java
+++ src/main/java/ua/reed/aws/nasa/pic/config/RdsIamDataSource.java
@@ -2,14 +2,12 @@ package ua.reed.aws.nasa.pic.config;
 
 import com.zaxxer.hikari.HikariDataSource;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
 import software.amazon.awssdk.services.rds.RdsClient;
 import software.amazon.awssdk.services.rds.RdsUtilities;
 import software.amazon.awssdk.services.rds.model.GenerateAuthenticationTokenRequest;
 
 import java.net.URI;
 
-@Component
 @RequiredArgsConstructor
 public class RdsIamDataSource extends HikariDataSource {
 
